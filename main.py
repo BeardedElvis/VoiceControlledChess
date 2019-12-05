ebb348326eb782a2dfe52ab2d36ac462ef8a70aa
Leo Wikström <wiko9723@gmail.com>
2019-12-05 01:54:53 +0100

    Just a stupid little starting point

---
+++ main.py
@@ -0,0 +1,10 @@
+from second import Response
+
+msg = "Hello there"
+print(msg)
+
+response = Response()
+print(response.getText())
+
+# Uncomment to pause before exiting
+# input("Press any key to continue...")
---
+++ second.py
@@ -0,0 +1,7 @@
+class Response:
+    
+    def __init__(self):
+        self.text = "General Kenobi"
+
+    def getText(self):
+        return self.text
